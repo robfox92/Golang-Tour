20e1ba790cd71c94b03d7ba18a9fa8cd6295a8c5
Robert Fox <fox.robert92@gmail.com>
2018-06-25 15:06:43 +0930

    added methods-with-pointer-receivers.go

---
+++ methodsInterfaces/methods-with-pointer-receivers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+
+// Use pointer receivers for either of the following cases:
+//  - Methods that modify the value that the receiver points to
+//  - When you want to avoid copying the value on each method call,
+//    for example when you are dealing with large structs.
+
+
+type Vertex struct {
+  X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+  v.X = v.X * f
+  v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+  return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+  v := &Vertex{3,4}
+  fmt.Printf("Before scaling: %+v, Abs: %v\n", v, v.Abs())
+  v.Scale(5)
+  fmt.Printf("After scaling: %+v, Abs: %v\n", v, v.Abs())
+}
