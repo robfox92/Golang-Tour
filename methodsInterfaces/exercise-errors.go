a0092c0d11aa16f7cec050ea3662462f47fb3fbf
Robert Fox <fox.robert92@gmail.com>
2018-06-30 15:12:26 +0800

    added framework for errors exercise

--- methodsInterfaces/exercise-errors.go
+++ methodsInterfaces/exercise-errors.go
@@ -5,8 +5,17 @@ import (
 )
 
 func Sqrt(x float64) (float64, error) {
-  return 0, nil
+  if x < 0{
+    return nil, 1
+  }
+  z := float64(1)
+  for i := 0; i < 10; i++ {
+      z -= (z*z - x)/(2*z)
+  }
+  return z, nil
 }
+
+// TODO: implement ErrNegativeSqrt and ErrNegativeSqrt.Error()
 
 func main() {
   fmt.Println(Sqrt(2))
