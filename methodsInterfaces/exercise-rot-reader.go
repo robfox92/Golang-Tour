051614514ee598afcea1aae3790bb7007433d983
Robert Fox <fox.robert92@gmail.com>
2018-06-30 15:42:02 +0800

    added incomplete exercise-rot-reader.go

--- methodsInterfaces/exercise-rot-reader.go
+++ methodsInterfaces/exercise-rot-reader.go
@@ -10,6 +10,11 @@ type rot13Reader struct {
   r io.Reader
 }
 
+func (r rot13Reader) Read([]byte) (n int, err error) {
+  return  n+13, nil
+}
+
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
   r := rot13Reader{s}
