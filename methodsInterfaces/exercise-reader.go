d36e9db71bbf2d86e16ceb7dcd398f6ff22485f2
Robert Fox <fox.robert92@gmail.com>
2018-06-30 15:39:09 +0800

    changed Read() to return 'A'

--- methodsInterfaces/exercise-reader.go
+++ methodsInterfaces/exercise-reader.go
@@ -5,7 +5,9 @@ import "golang.org/x/tour/reader"
 type MyReader struct {}
 
 // TODO: Add a Read([]byte) (int,error) method to MyReader
-
+func (m MyReader) Read([]byte) (int, error) {
+  return 'A', nil
+}
 
 func main() {
   reader.Validate(MyReader{})
